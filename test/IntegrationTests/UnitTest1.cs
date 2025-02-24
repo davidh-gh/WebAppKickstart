e207e690e9f9bc9625373729867c830ee3f82351
david <davidhrovat@gmail.com>
2025-02-24 08:13:54 +0100

    comment for aspire

--- test/IntegrationTests/UnitTest1.cs
+++ test/IntegrationTests/UnitTest1.cs
@@ -2,6 +2,9 @@
 
 public class UnitTest1
 {
+    // Follow the link below to write you tests with Aspire
+    // https://learn.microsoft.com/en-us/dotnet/aspire/testing/write-your-first-test?pivots=xunit
+
     [Fact]
     public void Test1()
     {

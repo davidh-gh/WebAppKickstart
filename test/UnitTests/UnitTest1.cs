c6535df160a83e8e223b4ef6897795bf39acdf00
david <davidhrovat@gmail.com>
2025-05-25 18:53:48 +0200

    Updated project dependencies

--- test/UnitTests/UnitTest1.cs
+++ test/UnitTests/UnitTest1.cs
@@ -1,11 +1,12 @@
-﻿namespace UnitTests;
+﻿using Shouldly;
+
+namespace UnitTests;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
-        Assert.True(true);
+        true.ShouldBeTrue();
     }
 }
